670c6c48d76132a481f526dd48ced821e7208c44
Andrew VanderHoff <andrewvanderhoff@gmail.com>
2016-06-25 18:26:28 -0400

    removed keys

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
 var keys = require('./keys.js');
 var mysql = require('mysql');
-var connection = mysql.createConnection(keys.mySQLKeys);
+//var connection = mysql.createConnection(keys.mySQLKeys);
+var connection = mysql.createConnection({
+	host:'localhost',
+	port:'3306',
+	user: 'root',
+	password: 'Deanie313581',
+	database: 'burgers_db'
 
+
+});
 
 connection.connect(function(err) {
     if (err) {
